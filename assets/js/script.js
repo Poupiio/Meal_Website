aa57c68f54d74379270bea87a60eea0bebc81466
Poupio <elise.maaiza33@gmail.com>
2023-03-04 21:39:10 +0100

    still have to add category name and actions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+        // CATEGORIES
 const fetchCat = async () => {
     const fetchResponse = await fetch("http://localhost:3000/api/categories");
     const response = await fetchResponse.json();
@@ -15,6 +16,10 @@ const displayCategories = async () => {
 
 displayCategories();
 
+
+// -------------------------------------------------------------------------- //
+
+        // ITEMS
 const fetchItems = async () => {
     const fetchResponse = await fetch("http://localhost:3000/api/items");
     const response = await fetchResponse.json();
@@ -23,65 +28,76 @@ const fetchItems = async () => {
 }
 
 const displayItems = async () => {
-    // Je fais un appel fetch vers mon API pour récupérer les données que je stocke dans la variable items
+    // Je fais un appel fetch vers mon API pour récupérer les données que je stocke dans les variables items et categories
     items = await fetchItems();
-
-    // Je cible la balise <tr> avec la class db-content
-    const container = document.querySelector('.db-content');
+    categories = await fetchCat();
 
     // Je boucle sur les données récupérées grâce à la fonction map()
     items.map((item) => {
         // Je viens recréer la structure HTML du tableau pour insérer les données de la BDD dynamiquement
 
-        // Création de la 1ère ligne
-        const firstRow = document.createElement('th');
-        firstRow.setAttribute('scope', 'row');
-        firstRow.textContent = item.name;
-        container.before(firstRow);
+        // Création d'une ligne => je crée une fonction pour ça
+        const createRow = () => {
+            const tbody = document.querySelector('.db-content');
+            const row = document.createElement('tr');
+            tbody.appendChild(row);
+            
+            const itemName = document.createElement('td');
+            // itemName.setAttribute('scope', 'row');
+            itemName.textContent = item.name;
+            row.append(itemName);
 
-        const description = document.createElement('td');
-        description.textContent = item.description;
-        container.before(description);
+            const description = document.createElement('td');
+            description.textContent = item.description;
+            row.append(description);
+    
+            const price = document.createElement('td');
+            price.textContent = item.price + "€";
+            row.append(price);
+            
+            // categories.map((category) => {
+            //     const cat = document.createElement('td');
+            //     cat.textContent = category.name;
+            //     row.append(cat);
 
-        const price = document.createElement('td');
-        price.textContent = item.price + "€";
-        container.before(price);
+            // })
 
-        const cat = document.createElement('td');
-        cat.textContent = item.category;
-        container.before(cat);
+            // const actions = document.createElement('td');
+            // row.append(actions);
+    
+            // Bouton Voir
+            // const show = document.createElement('a');
+            // show.setAttribute('href', 'assets/js/get.js');
+            // show.classList('btn');
+            // show.classList('btn-secondary');
+            // show.classList('actions');
+    
+            // const f1 = document.createElement('i');
+            // f1.classList('fa-solid fa-eye');
+            // f1.textContent = "Voir";
+    
+            // // Bouton Modifier
+            // const update = document.createElement('a');
+            // update.setAttribute('href', 'assets/js/update.js');
+            // update.classList('btn btn-secondary actions');
+    
+            // const f2 = document.createElement('i');
+            // f2.classList('fa-solid fa-square-pen');
+            // f2.textContent = "Modifier";
+    
+            // // Bouton Supprimer
+            // const remove = document.createElement('a');
+            // remove.setAttribute('href', 'assets/js/delete.js');
+            // remove.classList('btn btn-secondary actions');
+    
+            // const f3 = document.createElement('i');
+            // f3.classList('fa-solid fa-trash');
+            // f3.textContent = "Supprimer";
+    
+            // row.append(show, f1, update, f2, remove, f3);
+        };
 
-        // const actions = document.createElement('td');
-        // container.append(actions);
-
-        // // Bouton Voir
-        // const show = document.createElement('a');
-        // show.setAttribute('href', 'assets/js/get.js');
-        // show.classList('btn btn-secondary actions');
-
-        // const f1 = document.createElement('i');
-        // f1.classList('fa-solid fa-eye');
-        // f1.textContent = "Voir";
-
-        // // Bouton Modifier
-        // const update = document.createElement('a');
-        // update.setAttribute('href', 'assets/js/update.js');
-        // update.classList('btn btn-secondary actions');
-
-        // const f2 = document.createElement('i');
-        // f2.classList('fa-solid fa-square-pen');
-        // f2.textContent = "Modifier";
-
-        // // Bouton Supprimer
-        // const remove = document.createElement('a');
-        // remove.setAttribute('href', 'assets/js/delete.js');
-        // remove.classList('btn btn-secondary actions');
-
-        // const f3 = document.createElement('i');
-        // f3.classList('fa-solid fa-trash');
-        // f3.textContent = "Supprimer";
-
-        // container.append(show, f1, update, f2, remove, f3);
+        createRow();
     });
 };
 
